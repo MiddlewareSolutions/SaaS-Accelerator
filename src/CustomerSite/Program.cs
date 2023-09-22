bdc4d7910b05ccced041c70b4390f06cbb1fd0a3
timothe.rosaz <timothe.rosaz@middleware-solutions.fr>
2023-09-22 13:37:25 +0200

    Try to make logs work #3

--- src/CustomerSite/Program.cs
+++ src/CustomerSite/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
 namespace Marketplace.SaaS.Accelerator.CustomerSite;
 
 /// <summary>
